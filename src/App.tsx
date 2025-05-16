641670491923108542f27ab9edc97e3815bbd278
Ivan Orekhov <ivanorekhov04022005@gmail.com>
2025-05-16 22:27:44 +0300

    add h2

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
 
   return (
-    <>
-      Решение находятся в ветках: manual-case И regexp-case
-    </>
+    <div style={{textAlign:'center', margin: '100px', fontSize:'20px'}}>
+      Решение находятся в ветках: <h2>tree-parser</h2> и <h2> regexp-parser</h2>
+    </div>
   )
 }
 
